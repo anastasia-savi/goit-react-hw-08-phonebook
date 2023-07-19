eb2dd6c21d26858de3afc642fe94ae2635a946c0
Anastasia <104596018+anastasia-savi@users.noreply.github.com>
2023-07-19 15:32:09 +0200

    hw-react++

--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import TextField from '@mui/material/TextField';
 import PropTypes from 'prop-types';
 import css from './Filter.module.css';
 
 const Filter = ({ filter, onChange }) => {
   return (
     <label className={css.label}>
-      <span className={css.span}>Find contacts by name</span>
-      <input type="text" value={filter} onChange={onChange} />
+      <TextField
+        id="standard-basic"
+        label="Find contacts by name"
+        variant="standard"
+        type="text"
+        value={filter}
+        onChange={onChange}
+      />
     </label>
   );
 };
