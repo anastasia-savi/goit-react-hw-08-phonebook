85f71b40b57efc03f61564018801cb53cad9dabb
Anastasia <104596018+anastasia-savi@users.noreply.github.com>
2023-07-20 15:29:51 +0200

    hw

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -25,20 +25,6 @@ export default class App extends React.Component {
     }
   };
 
-  componentDidMount() {
-    const saveContacts = localStorage.getItem('contacts');
-    const parse = JSON.parse(saveContacts);
-    if (parse) {
-      this.setState({ contacts: parse });
-    }
-  }
-
-  componentDidUpdate(prevProps, prevState) {
-    if (this.state.contacts !== prevState.contacts) {
-      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
   changeFilter = event => {
     this.setState({ filter: event.currentTarget.value });
   };
