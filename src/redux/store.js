e317e68bcb2abc2ee17f155a33dbcd6f02e2e940
Anastasia <104596018+anastasia-savi@users.noreply.github.com>
2023-08-31 14:01:21 +0200

    hw8

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,40 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { contactsReducer } from './contactSlice';
 import { filterReducer } from './filterSlice';
+import {
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
 
 export const store = configureStore({
   reducer: {
     contacts: contactsReducer,
     filter: filterReducer,
   },
-
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
+
+export const persistor = persistStore(store);
+
+
+// import { configureStore } from '@reduxjs/toolkit';
+// import { contactsReducer } from './contactSlice';
+// import { filterReducer } from './filterSlice';
+
+// export const store = configureStore({
+//   reducer: {
+//     contacts: contactsReducer,
+//     filter: filterReducer,
+//   },
+
+// });
